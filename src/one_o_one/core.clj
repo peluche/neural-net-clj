9afffcb01cf233afdd042a1aac00ab1a1fa1dbfa
peluche <peluche@example.com>
2014-03-08 04:53:56 +0800

    cleaning

--- src/one_o_one/core.clj
+++ src/one_o_one/core.clj
@@ -7,6 +7,11 @@
 
 (set-current-implementation :vectorz)
 
+;; ==========
+;; TODO-list
+;; ==========
+;; - add debug maccros to check that parameters are (matrix :vectorz)
+
 (defn activation-function
   "the sigmoid activation function"
   [x]
@@ -60,15 +65,9 @@
   (let [biais    (matrix [[1]]) ;; i could shave some perf by making this global as it never changes
         in-biais (join biais in)
         out      (mmul theta in-biais)]
-    ;; inplace modification because:
+    ;; could use inplace modification (emap!) because:
     ;; if a tree fall and no one see it, it doesn't make noise !
-    ;; (println "class in:      " (class in))
-    ;; (println "class in-biais:" (class in-biais))
-    ;; (println "class out:     " (class out))
     (emap activation-function out)))
-
-;; (let [a (matrix [[1]])]
-;;   (emap! inc (mmul a a)))
 
 (defn forward-propagation
   "run forward-propagation on a neural network with the given input
@@ -86,7 +85,6 @@
   ;; make sure "expected" is a matrix ?
   (- out expected))
 
-;; (keys (ns-publics 'clojure.core.matrix))
 
 (defn- backward-propagation-rec
   "calculates the deltas for each layer"
@@ -144,7 +142,6 @@
             vals       (cons in (reverse prop))
             big-deltas (calc-delta-net net deltas vals rate)
             new-net    (map - net big-deltas)
-            ;; new-net    (clojure.core.matrix.operators/- net big-deltas)
             ;; _ (println "deltas 0:" (class (nth deltas 0)))
             ;; _ (println "deltas 1:" (class (nth deltas 1)))
             ;; _ (println "vals 0:" (class (nth vals 0)))
@@ -177,11 +174,8 @@
         prop (forward-propagation net in) ; [ goog type ]
         deltas (backward-propagation net prop out) ; first bad
         ]
-    (time (println (class (nth deltas 1)))
-     ))
-; CCC
+    (time (println (class (nth deltas 1)))))
 
-;; XXX
 (defn aaa []
   (let [iter 1
         in  (matrix (array (repeat 400 [1])))
@@ -190,117 +184,21 @@
         prop (forward-propagation net in) ; [ goog type ]
         deltas (backward-propagation net prop out) ; first bad
         ]
-    (time (println (class (nth deltas 1)))
-          )))
+    (time (println (class (nth deltas 1)))))))
 
 
-  (fn []
-    )
+;; ==================
+;; benchmark memory
+;; ==================
+;; (let [gc       (fn [] (dotimes [_ 4] (System/gc)))
+;;       used-mem (fn []
+;;                  (let [runtime (Runtime/getRuntime)]
+;;                    (gc)
+;;                    (- (.totalMemory runtime) (.freeMemory runtime))))
+;;       before (used-mem)
+;;       _      (make-neural-network [400 400 1000])
+;;       after  (used-mem)]
+;;   (println "mem: ~" (- after before 24) "bytes"))
 
-;;   (time
-;;    (make-neural-network [400 400 400]))
-
-  ;; (defn gc []
-  ;;   (dotimes [_ 4] (System/gc)))
-
-  ;; (defn used-memory []
-  ;;   (let [runtime (Runtime/getRuntime)]
-  ;;     (gc)
-  ;;     (- (.totalMemory runtime) (.freeMemory runtime))))
-
-  ;; (let [gc       (fn [] (dotimes [_ 4] (System/gc)))
-  ;;       used-mem (fn []
-  ;;                  (let [runtime (Runtime/getRuntime)]
-  ;;                    (gc)
-  ;;                    (- (.totalMemory runtime) (.freeMemory runtime))))
-  ;;       before (used-mem)
-  ;;       _      (make-neural-network [400 400 1000])
-  ;;       after  (used-mem)]
-  ;;   (println "mem: ~" (- after before 24) "bytes"))
-
-  ;; (sizeof)
-
-  ;; (def a (make-neural-network [400 400 400]))
-  ;; (mapv shape a)
-
-  ;; (def in (matrix (repeat 400 [1])))
-  ;; (def layer1 (matrix (make-random-matrix 400 401)))
-  ;; (def layer2 (matrix (make-random-matrix 400 400)))
-  ;; (time (mmul layer1 (cons [1] in)))
-  ;; (time (mmul layer1 (matrix (cons [1] in))))
-  ;; (time (mmul layer1 (join [[1]] in)))
-
-  ;; (time (matrix in))
-  ;; (def in2 (repeat 400 [1]))
-  ;; in2
-  ;; (time (matrix in2))
-  ;; (time (cons [1] in))
-
-  ;; (time (mmul layer2 in))
-  ;; (shape in)
-  ;; (shape layer2)
-
-  ;; (time (dotimes [n 10000]
-  ;;         (mmul layer2 in)))
-  
-  ;; (time (dotimes [n 100]
-  ;;         (mmul (transpose layer2))))
-
-
-
-  ;; (vector-of)
-  ;; vec
-
-
-  ;; (time (mmul (cons [1] in) layer1))
-
-  ;; (let [in     (repeat 400 [1])
-  ;;       layer1 (make-random-matrix 400 401)]
-  ;;   (time (mmul (cons [1] in) layer1))
-  ;;   nil)
-
-  ;; =====================
-  ;; manual testing
-  ;; =====================
-  ;; (fn []
-  ;;   (let [net        (unserialize-neural-network "/tmp/test.net")
-  ;;         in         [[1] [0]]
-  ;;         rate       1
-  ;;         prop       (forward-propagation net in)
-  ;;         expected   [[0] [1]]
-  ;;         error      (cost-output (first prop) expected)
-  ;;         deltas     (backward-propagation net prop expected)
-  ;;         _ (println "backward-propagation: " deltas)
-  ;;         vals       (cons in (reverse prop))
-  ;;         big-deltas (calc-delta-net net deltas vals rate)
-  ;;         new-net    (clojure.core.matrix.operators/- net big-deltas)]
-  
-  ;;     (println "res: ")
-  ;;     (pm (first prop))
-  ;;     (println "net: ")
-  ;;     (pm (first net))
-  ;;     (println "big-deltas: ")
-  ;;     (pm (first big-deltas))
-  ;;     (println "new-net: ")
-  ;;     (pm (first new-net))
-  ;;     (println "next-res: ")
-  ;;     (pm (first (forward-propagation new-net in)))))
-
-  (defn mmm [iter]
-    (let [iter 1
-          in  (array (repeat 400 [1]))
-          out (array (repeat 400 [0.5]))
-          net (make-neural-network [400 400 400])]
-      (time (do
-              (helper-test-loop in out 0.1 iter net)
-              nil))))
-
-
-  ;; =====================
-  ;; broadcast
-  ;; =====================
-  ;; (* [[1 2 3 4]  
-  ;;     [5 6 7 8]]
-  ;;    (mapv (fn [[x]] (repeat 4 x))
-  ;;          [[2] [3]]))
-  )
+;; (vector-of)
+;; (keys (ns-publics 'clojure.core.matrix))
